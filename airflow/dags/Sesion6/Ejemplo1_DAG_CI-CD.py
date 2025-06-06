b57eba633e5464c1fa714c225799d52fbbb8ec2c
Robert Huayta Cortez <46381414+RobertHC85@users.noreply.github.com>
2025-06-06 02:05:45 +0000

    Actualiza DAG Ejemplo1_DAG_CI-CD para CI/CD

---
+++ airflow/dags/Sesion6/Ejemplo1_DAG_CI-CD.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="DAG_ci_cd_prueba",
+    start_date=datetime(2025, 1, 1),
+    schedule_interval="@daily",
+    catchup=False,
+    description="DAG de prueba para validar CI/CD en Codespace",
+    tags=["ci_cd", "prueba"],
+) as dag:
+
+    tarea_prueba = BashOperator(
+        task_id="tarea_de_prueba",
+        bash_command="echo 'CI/CD ejecutado correctamente en DAG_ci_cd_prueba'",
+    )
